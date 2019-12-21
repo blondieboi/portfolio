f80b0d78411b7fe9f9a58c0d53dbc942ddafb84a
Alexandre Blondot <alexandre.blondot@gmail.com>
2019-12-21 17:18:35 +0100

    refactored image gallery

--- src/Views/Gallery.jsx
+++ src/Views/Gallery.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "../Styles/Gallery.css";
-import image1 from "../Assets/Gallery/image1.png";
-import image2 from "../Assets/Gallery/image2.png";
-import image3 from "../Assets/Gallery/image3.png";
-import image4 from "../Assets/Gallery/image4.png";
-import image5 from "../Assets/Gallery/image5.png";
-import image6 from "../Assets/Gallery/image6.png";
+
+function importAll(r) {
+	return r.keys().map(r);
+}
+const images = importAll(
+	require.context("../Assets/Gallery", false, /\.(png)$/)
+);
+
+const imageGallery = images.map(image => (
+	<img key={image} alt={image} src={image} />
+));
 
 const Gallery = () => {
 	return (
 		<div className="gallery-wrapper" id="gallery">
-			<img src={image1} alt="" />
-			<img src={image2} alt="" />
-			<img src={image3} alt="" />
-			<img src={image4} alt="" />
-			<img src={image5} alt="" />
-			<img src={image6} alt="" />
+			{imageGallery}
 		</div>
 	);
 };
