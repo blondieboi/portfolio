97e8542b81c7ca18a7bc4097ceb27e48a4108576
Alexandre Blondot <alexandre.blondot@gmail.com>
2019-12-21 22:53:39 +0100

    added modal

--- src/Views/Gallery.jsx
+++ src/Views/Gallery.jsx
@@ -9,13 +9,33 @@ const images = importAll(
 );
 
 const imageGallery = images.map(image => (
-	<img key={image} alt={image} src={image} />
+	<img
+		key={image}
+		alt={image}
+		src={image}
+		onClick={() => {
+			document.getElementById("myModal").style.display = "block";
+			document.getElementById("img01").src = image;
+		}}
+		className="gallery-image"
+	/>
 ));
 
 const Gallery = () => {
 	return (
 		<div className="gallery-wrapper" id="gallery">
 			{imageGallery}
+			<div id="myModal" className="modal">
+				<span
+					className="close"
+					onClick={() => {
+						document.getElementById("myModal").style.display = "none";
+					}}
+				>
+					&times;
+				</span>
+				<img className="modal-content" id="img01" />
+			</div>
 		</div>
 	);
 };
